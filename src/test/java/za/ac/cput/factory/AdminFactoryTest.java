501dfe695f829a475308338b3d6a504411b25f1c
TylorTazz2024 <165163298+TylorTazz2024@users.noreply.github.com>
2025-05-18 21:32:24 +0200

    updated/Test

---
+++ src/test/java/za/ac/cput/factory/AdminFactoryTest.java
@@ -0,0 +1,13 @@
+package za.ac.cput.factory;
+
+import static org.junit.jupiter.api.Assertions.*;
+class AdminFactoryTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
---
+++ src/test/java/za/ac/cput/factory/UserFactoryTest.java
@@ -0,0 +1,13 @@
+package za.ac.cput.factory;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserFactoryTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
